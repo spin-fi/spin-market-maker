f06f8abfc91ae4d7e59198efc5ea8e0e2d4937df
Leonid Chedrik <chedrik.leonid@gmail.com>
2022-06-20 17:48:42 +0300

    Update pricer

--- src/price/index.ts
+++ src/price/index.ts
@@ -1,9 +1,12 @@
 import config from '../configs/config.js';
 import { WebsocketUpdates } from './ccxws.js';
 import { RefPricer } from './ref.js';
+import BigNumber from 'bignumber.js';
 
 const watcher = new WebsocketUpdates();
 const refWatcher = new RefPricer();
+const secondRefWatcher = new RefPricer();
+
 export async function getLastPrice(): Promise<number> {
   const priceSource = config.get('price.source');
 
@@ -12,7 +15,18 @@ export async function getLastPrice(): Promise<number> {
   }
 
   if (priceSource === 'ref') {
-    return await refWatcher.getPrice();
+    const poolId = +config.get('price.source_ticker').split(', ')[0];
+    const secondPoolId = +config.get('price.source_ticker').split(', ')[1] || 0;
+
+    if (!secondPoolId) {
+      return await refWatcher.getPrice(poolId);
+    } else {
+      const pool_1_price = await refWatcher.getPrice(poolId);
+      const pool_2_price = await secondRefWatcher.getPrice(secondPoolId);
+      return new BigNumber(pool_2_price)
+        .div(new BigNumber(pool_1_price))
+        .toNumber();
+    }
   }
 
   return 0;
