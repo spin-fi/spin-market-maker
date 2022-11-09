4c92681784c69655bf6704c5843ca6c9100aaa85
Leonid Chedrik <chedrik.leonid@gmail.com>
2022-11-09 22:43:52 +0300

    ref-sdk

--- src/price/index.ts
+++ src/price/index.ts
@@ -1,15 +1,14 @@
 import config from '../configs/config.js'
 import { WebsocketUpdates } from './ccxws.js'
-import { RefPricer } from './ref.js'
-import { JumboPricer } from './jumbo.js'
+import { RefPricer } from './ref-sdk.js'
 import BigNumber from 'bignumber.js'
+import { Market } from '@spinfi/core'
 
 const watcher = new WebsocketUpdates()
 const refWatcher = new RefPricer()
 const secondRefWatcher = new RefPricer()
-const jumboWatcher = new JumboPricer()
 
-export async function getLastPrice(): Promise<number> {
+export async function getLastPrice(spinMarket?: Market): Promise<number> {
   const priceSource = config.get('price.source')
 
   if (['binance', 'ftx'].includes(priceSource)) {
@@ -21,20 +20,12 @@ export async function getLastPrice(): Promise<number> {
     const secondPoolId = +config.get('price.source_ticker').split(', ')[1] || 0
 
     if (!secondPoolId) {
-      return await refWatcher.getPrice(poolId)
+      return await refWatcher.getPrice(poolId, spinMarket)
     } else {
-      const pool_1_price = await refWatcher.getPrice(poolId)
-      const pool_2_price = await secondRefWatcher.getPrice(secondPoolId)
+      const pool_1_price = await refWatcher.getPrice(poolId, spinMarket)
+      const pool_2_price = await secondRefWatcher.getPrice(secondPoolId, spinMarket)
       return new BigNumber(pool_2_price).div(new BigNumber(pool_1_price)).toNumber()
     }
-  }
-
-  if (priceSource === 'jumbo') {
-    const poolId = +config.get('price.source_ticker').split(', ')[0]
-    // nearX
-    const nearXPrice = await jumboWatcher.getPrice(poolId, true)
-    const nearPrice = await refWatcher.getPrice(3)
-    return new BigNumber(nearXPrice).multipliedBy(new BigNumber(nearPrice)).toNumber()
   }
 
   return 0
